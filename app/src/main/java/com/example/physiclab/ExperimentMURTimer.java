39779cfbeb578fd5a3a7266784563b1ae159983a
Luis Miguel Montes <luismiguel010@gmail.com>
2020-07-25 11:14:31 -0500

    Already 5 times in textView and show only seconds with milliseconds

--- app/src/main/java/com/example/physiclab/ExperimentMURTimer.java
+++ app/src/main/java/com/example/physiclab/ExperimentMURTimer.java
@@ -12,14 +12,24 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.onsets.OnsetHandler;
+import be.tarsos.dsp.onsets.PercussionOnsetDetector;
+
 public class ExperimentMURTimer extends AppCompatActivity {
 
     Button btnStart, btnPause, btnLap;
-    TextView textTimer1;
+    TextView textState, time1, time2, time3, time4, time5;
+    String time;
     Handler customHandler = new Handler();
     LinearLayout container;
-
     long startTime=0L, timeInMilliseconds=0L, timeSwapBuff=0L, updateTime=0L;
+    AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
+    double threshold = 8;
+    double sensitivity = 50;
+    boolean isFirstTime = true;
+    int counterClap = 0;
 
     Runnable updateTimetThread = new Runnable() {
         @Override
@@ -28,10 +38,8 @@ public class ExperimentMURTimer extends AppCompatActivity {
             updateTime = timeSwapBuff + timeInMilliseconds;
             int secs = (int)(updateTime / 1000);
             int mins = secs / 60;
-            secs%=60;
             int milliseconds = (int) (updateTime%1000);
-            textTimer1.setText("" + mins + ":" + String.format("%2d", secs) + ":"
-            + String.format("%3d", milliseconds));
+            time = secs + "," + String.format("%3d", milliseconds);
             customHandler.postDelayed(this,0);
         }
     };
@@ -41,11 +49,23 @@ public class ExperimentMURTimer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experiment_m_u_r_timer);
 
-        btnStart = (Button)findViewById(R.id.btnStart);
-        btnPause = (Button)findViewById(R.id.btnPause);
-        btnLap = (Button)findViewById(R.id.btnLap);
-        textTimer1 = (TextView) findViewById(R.id.timerValue1);
-        container = (LinearLayout) findViewById(R.id.container);
+        initComponents();
+
+        PercussionOnsetDetector mPercussionDetector = new PercussionOnsetDetector(22050, 1024,
+                new OnsetHandler() {
+                    @Override
+                    public void handleOnset(double time, double salience) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                controlTime(counterClap);
+                            }
+                        });
+                    }
+                }, sensitivity, threshold);
+
+        dispatcher.addAudioProcessor(mPercussionDetector);
+        new Thread(dispatcher, "Audio Dispatcher").start();
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,20 +80,71 @@ public class ExperimentMURTimer extends AppCompatActivity {
             public void onClick(View view) {
                 timeSwapBuff += timeInMilliseconds;
                 customHandler.removeCallbacks(updateTimetThread);
+                textState.setText("Paused");
             }
         });
 
         btnLap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View addView = inflater.inflate(R.layout.time_mur_sound,null);
-                TextView txtValue = (TextView)addView.findViewById(R.id.txtContent);
-                txtValue.setText(textTimer1.getText());
-                container.addView(addView);
+                restartTime();
             }
         });
+    }
 
+    private void initComponents() {
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnPause = (Button)findViewById(R.id.btnPause);
+        btnLap = (Button)findViewById(R.id.btnLap);
+        textState = (TextView) findViewById(R.id.txtState);
+        time1 = (TextView)findViewById(R.id.time1);
+        time2 = (TextView)findViewById(R.id.time2);
+        time3 = (TextView)findViewById(R.id.time3);
+        time4 = (TextView)findViewById(R.id.time4);
+        time5 = (TextView)findViewById(R.id.time5);
+    }
+
+    public void controlTime(int counter){
+        switch (counter){
+            case 0:
+                textState.setText("Started");
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimetThread, 0);
+                counterClap = 1;
+                break;
+            case 1:
+                time1.setText(time);
+                counterClap = 2;
+                break;
+            case 2:
+                time2.setText(time);
+                counterClap = 3;
+                break;
+            case 3:
+                time3.setText(time);
+                counterClap = 4;
+                break;
+            case 4:
+                time4.setText(time);
+                counterClap = 5;
+                break;
+            case 5:
+                time5.setText(time);
+                break;
+        }
+    }
+
+    public void restartTime(){
+        counterClap = 0;
+        time1.setText("");
+        time2.setText("");
+        time3.setText("");
+        time4.setText("");
+        time5.setText("");
+        startTime = 0L;
+        timeInMilliseconds=0L;
+        timeSwapBuff=0L;
+        updateTime=0L;
     }
 
 
